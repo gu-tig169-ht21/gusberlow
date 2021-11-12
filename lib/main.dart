f11ea62c7b698f89cb5807a5cd1ab4e19e402073
berglundlovisa <63313681+berglundlovisa@users.noreply.github.com>
2021-11-12 14:45:20 +0100

    La till resterande kod

--- lib/main.dart
+++ lib/main.dart
@@ -15,3 +15,112 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MainView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text('TIG169 TODO'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _list(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SecondView()));
+        },
+        child: const Icon(Icons.add, color: Colors.white, size: 42),
+      ),
+    );
+  }
+
+  Widget _list() {
+    return ListView(
+      children: [
+        _item('Köpa Körv'),
+        _item('Gå till frissan'),
+        _item('Kidnappa vovve'),
+        _item('Lyfta skrot'),
+        _item('Hugga ved'),
+      ],
+    );
+  }
+
+  Widget _item(strText) {
+    return ListTile(
+      leading: Icon(Icons.check_box_outline_blank),
+      title: Text(strText),
+      trailing: Icon(Icons.highlight_remove),
+    );
+  }
+}
+
+Widget _checkboxRow() {
+  return Container(
+    margin: EdgeInsets.only(top: 25, bottom: 50),
+    child: Row(
+      children: [
+        Checkbox(
+          value: false,
+          onChanged: (val) {},
+        ),
+        const Text('Write a book'),
+        Container(width: 250),
+        const Icon(Icons.close),
+      ],
+    ),
+  );
+}
+
+class SecondView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Nya TODO')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(height: 20),
+            _nameInputField(),
+            Container(height: 20),
+            _addText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _nameInputField() {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16),
+      child: TextField(
+        decoration: InputDecoration(hintText: 'Skriv ny TODO'),
+      ),
+    );
+  }
+
+  Widget _addText() {
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 180),
+          child: Icon(Icons.add),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 5),
+          child: Text('Addd'),
+        ),
+      ],
+    );
+  }
+}
